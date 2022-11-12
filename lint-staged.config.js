e9da1e8a5a7923efb125daf3efe694c0bb30b38b
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2022-11-12 02:35:54 -0800

    enhanced husky

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': filenames => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+  ],
+  '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
+  'src/translations/*.(json)': filenames => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+  ],
+};
